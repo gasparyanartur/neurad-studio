44b0b400c60c26541f0da60cdcbda93a75013fed
Artur Gasparyan <gasparyanartur99@gmail.com>
2024-06-06 15:08:03 +0200

    Improve diffusion training loop

--- nerfstudio/scripts/hal_render_batch_loop.sh
+++ nerfstudio/scripts/hal_render_batch_loop.sh
@@ -1,5 +1,5 @@
-config_neurad="/staging/agp/masterthesis/nerf-thesis-shared/output/neurad_imaginedriving/pandaset-imaginedriving/331981/imaginedriving-001-331981-1/imaginedriving/2024-05-25_130746/config.yml"
-config_diffusion="/staging/agp/masterthesis/nerf-thesis-shared/output/neurad_imaginedriving/pandaset-imaginedriving/331982/imaginedriving-001-331982-1/imaginedriving/2024-05-25_130744/config.yml"
+config_neurad="/staging/agp/masterthesis/nerf-thesis-shared/output/neurad_imaginedriving/pandaset-imaginedriving/331982/imaginedriving-001-331982-1/imaginedriving/2024-05-25_130744/config.yml"
+config_diffusion="/staging/agp/masterthesis/nerf-thesis-shared/output/neurad_imaginedriving/pandaset-imaginedriving/331981/imaginedriving-001-331981-1/imaginedriving/2024-05-25_130746/config.yml"
 config_diffusion_lora="/staging/agp/masterthesis/nerf-thesis-shared/output/neurad_imaginedriving/pandaset-imaginedriving/332043/imaginedriving-001-332043-1/imaginedriving/2024-05-26_174218/config.yml"
 
 
