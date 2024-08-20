1a83b71ae9bcac6ad545400b473c2b8d8683a311
Artur Gasparyan <gasparyanartur99@gmail.com>
2024-08-20 06:59:04 +0200

    add updated config

--- nerfstudio/scripts/hal_lora_train_single_scene.sh
+++ nerfstudio/scripts/hal_lora_train_single_scene.sh
@@ -3,8 +3,8 @@
 #SBATCH --nodes 1
 #SBATCH --gpus 1
 #SBATCH --cpus-per-task 8
-#SBATCH --output /staging/agp/masterthesis/nerf-thesis-shared/logs/train-lora/slurm/%j.out
-#SBATCH --partition=zprod 
+#SBATCH --output /staging/agp/masterthesis/nerf-thesis-shared/logs/train_lora/slurm/%j.out
+#SBATCH --partition=zprodlow
 #SBATCH --job-name=finetune
 
 if [ -z ${WANDB_API_KEY} ]; then
@@ -24,7 +24,9 @@ mixed_precision=${MIXED_PRECISION:-"no"}
 #singularity exec --env PYTHONPATH=$workdir --env WANDB_API_KEY=$wandb_api_key --bind /staging:/staging -H $workdir --nv $image_path accelerate launch --num_processes=$num_processes --num_machines=$num_machines --dynamo_backend=$dynamo_backend --mixed_precision=$mixed_precision --main_process_port=$main_process_port scripts/run_train_lora.py $config_path
 singularity exec --nv \
     --bind $PWD:/nerfstudio \
-    --bind /home:/home \
+    --bind /staging:/staging  \
+    --bind /workspaces:/workspaces \
+    --bind /datasets:/datasets \
     --env PYTHONPATH=/nerfstudio \
     --env WANDB_API_KEY=$WANDB_API_KEY \
     --env LORA_TRAIN_CONFIG=$lora_train_config \
