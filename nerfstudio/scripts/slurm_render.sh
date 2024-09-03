ff15374e1d202023ebea0e7b48129362c40e25d2
Artur Gasparyan <gasparyanartur99@gmail.com>
2024-09-04 00:56:52 +0200

    update render

--- nerfstudio/scripts/slurm_render.sh
+++ nerfstudio/scripts/slurm_render.sh
@@ -98,7 +98,7 @@ if [[ -z ${x_shift} ]]; then
 fi
 
 
-output_path=$output_dir/$model_name/$scene/$cameras/${x_shift}m/${job_id}_${run_name}
+output_path=$output_dir/$model_name/$scene/${cameras}_camera/${x_shift}m/${job_id}_${run_name}
 mkdir -p $output_path
 
 echo "Starting renderings with job_id ${job_id}"
