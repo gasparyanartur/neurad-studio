190ec6171aed0e56541e12deba78b658ab2cf141
Artur Gasparyan <gasparyanartur99@gmail.com>
2024-10-27 18:37:21 +0100

    add run viser script

---
+++ nerfstudio/scripts/slurm_viewer.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#SBATCH --job-name=render
+#SBATCH --nodes 1
+#SBATCH --gpus 1
+#SBATCH --cpus-per-task 32
+#SBATCH --mem 100G
+#SBATCH --output logs/%x/slurm/%j.out
+#SBATCH --array=0
+#SBATCH --time 2-00:00:00
+
+source .env
+
+name=${NAME:-"viewer"}
+
+image_path=${IMAGE_PATH:-"containers/neurad_140824.sif"}
+if [ -z ${NERF_CONFIG_PATH} ]; then
+    echo "NERF_CONFIG_PATH not set. Exiting."
+    exit 1;
+fi
+
+
+#BIND_CMD="--bind /staging:/staging --bind /workspaces:/workspaces --bind /datasets:/datasets"
+BIND_CMD="--bind /proj:/proj --bind /home:/home"
+
+execute="singularity exec \
+            --nv \
+            $BIND_CMD \
+            --home /nerfstudio \
+            --bind $PWD:/nerfstudio \
+            --env PYTHONPATH=/nerfstudio \
+            --env WANDB_API_KEY=$WANDB_API_KEY \
+            $image_path"
+
+$execute python3.10 -u nerfstudio/scripts/viewer/run_viewer.py \
+    --load-config $NERF_CONFIG_PATH \
+    $@ 
+
+chmod 775 -R $output_path
+
+#
+#EOF
